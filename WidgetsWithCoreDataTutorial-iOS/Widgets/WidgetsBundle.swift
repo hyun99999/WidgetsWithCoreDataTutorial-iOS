d8455fac1e4863dba5054414453fdbab581c9465
hyun99999 <ypsophila97@icloud.com>
2022-11-18 19:25:36 +0900

    chore: set project

---
+++ WidgetsWithCoreDataTutorial-iOS/Widgets/WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by kimhyungyu on 2022/11/18.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+        WidgetsLiveActivity()
+    }
+}
---
+++ WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetsWithCoreDataTutorial-iOS
+//
+//  Created by kimhyungyu on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
