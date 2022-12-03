8c6525079f63d8ef687e8eaa32a30bd89b2adb1d
hyun99999 <ypsophila97@icloud.com>
2022-12-03 21:47:40 +0900

    feat: implement IntentHandler that conforms SelectMyCardIntentHandling

--- WidgetsWithCoreDataTutorial-iOS/IntentsExtension/IntentHandler.swift
+++ WidgetsWithCoreDataTutorial-iOS/IntentsExtension/IntentHandler.swift
@@ -8,6 +8,20 @@
 import Intents
   
 class IntentHandler: INExtension, SelectMyCardIntentHandling {
+    func provideMyCardOptionsCollection(for intent: SelectMyCardIntent, with completion: @escaping (INObjectCollection<Card>?, Error?) -> Void) {
+        let myCards: [Card] = MyCardDetail.availableMyCards.map { card in
+            let myCard = Card(identifier: card.cardName, display: card.cardName)
+            myCard.cardName = card.cardName
+            return myCard
+        }
+        
+        // ✅ Create a collection with the array of characters.
+        let collection = INObjectCollection(items: myCards)
+        
+        // ✅ Call the completion handler, passing the collection.
+        completion(collection, nil)
+    }
+    
     override func handler(for intent: INIntent) -> Any {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
