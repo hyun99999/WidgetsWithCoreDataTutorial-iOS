399a7fa9a372f5294c70700f6f39dcca7b863c92
hyun99999 <ypsophila97@icloud.com>
2022-12-05 13:15:28 +0900

    feat: add ViewController outlet property

--- WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/Source/ViewController/ViewController.swift
+++ WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/Source/ViewController/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Components
+    @IBOutlet weak var backgorundImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+// TODO: - Core Data 데이터 저장.
 }
 
+// MARK: - Extension
+extension ViewController {
+    
+}
