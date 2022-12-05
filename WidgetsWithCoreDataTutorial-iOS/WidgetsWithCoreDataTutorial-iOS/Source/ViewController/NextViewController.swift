5669bde8f66a0b2dcd69c2441d10723a7bbdf347
hyun99999 <ypsophila97@icloud.com>
2022-12-05 13:13:52 +0900

    chore: add NextViewController

---
+++ WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/Source/ViewController/NextViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NextViewController.swift
+//  WidgetsWithCoreDataTutorial-iOS
+//
+//  Created by kimhyungyu on 2022/12/04.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
