b138151df0dd383d71b09b7eff304a8fcb8fcb2b
hyun99999 <ypsophila97@icloud.com>
2022-12-14 01:29:37 +0900

    fix: fix MyCardDetail data model's contents

--- WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/Source/Model/MyCardDetail.swift
+++ WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/Source/Model/MyCardDetail.swift
@@ -12,10 +12,9 @@ struct MyCardDetail {
     let cardName: String
     let userName: String
     let cardImage: UIImage
-//    let cardImage: Data
     
     static let availableMyCards = [
-        MyCardDetail(cardName: "첫번째 카드", userName: "첫현규", cardImage: UIImage(named: "background") ?? UIImage()),
-        MyCardDetail(cardName: "두번째 카드", userName: "두현규", cardImage: UIImage(named: "imgCardWidget") ?? UIImage())
+        MyCardDetail(cardName: "첫 번째 카드", userName: "첫현규", cardImage: UIImage(named: "background") ?? UIImage()),
+        MyCardDetail(cardName: "두 번째 카드", userName: "두현규", cardImage: UIImage(named: "imgCardWidget") ?? UIImage())
     ]
 }
