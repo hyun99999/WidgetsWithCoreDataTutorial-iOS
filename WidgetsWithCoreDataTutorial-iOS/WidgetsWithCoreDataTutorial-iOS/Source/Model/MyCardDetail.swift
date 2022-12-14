1162c98b9201ba958494b4b847f65eb7bbc4f8d8
hyun99999 <ypsophila97@icloud.com>
2022-12-14 11:28:29 +0900

    feat: MyCardDetail set target membership that useable in WidgetsExtension

--- WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/Source/Model/MyCardDetail.swift
+++ WidgetsWithCoreDataTutorial-iOS/WidgetsWithCoreDataTutorial-iOS/Source/Model/MyCardDetail.swift
@@ -13,9 +13,9 @@ struct MyCardDetail {
     let userName: String
     let cardImage: UIImage
     
-    // CoreDataManager 를 사용하면서 mock data 는 더 이상 필요하지 않게되었다.
-//    static let availableMyCards = [
-//        MyCardDetail(cardName: "첫 번째 카드", userName: "첫현규", cardImage: UIImage(named: "background") ?? UIImage()),
-//        MyCardDetail(cardName: "두 번째 카드", userName: "두현규", cardImage: UIImage(named: "imgCardWidget") ?? UIImage())
-//    ]
+    // Widget preview 를 위해서 정적 데이터를 남겨두었다.
+    static let availableMyCards = [
+        MyCardDetail(cardName: "첫 번째 카드", userName: "첫현규", cardImage: UIImage(named: "background") ?? UIImage()),
+        MyCardDetail(cardName: "두 번째 카드", userName: "두현규", cardImage: UIImage(named: "imgCardWidget") ?? UIImage())
+    ]
 }
