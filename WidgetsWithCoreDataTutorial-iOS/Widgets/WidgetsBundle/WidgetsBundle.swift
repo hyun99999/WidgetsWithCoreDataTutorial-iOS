3cbe6168881de968484dbf590c35a8818402b132
hyun99999 <ypsophila97@icloud.com>
2023-01-13 18:53:19 +0900

    add: add widget to WidgetBundle for display OpenAppLockScreenWidget

--- WidgetsWithCoreDataTutorial-iOS/Widgets/WidgetsBundle/WidgetsBundle.swift
+++ WidgetsWithCoreDataTutorial-iOS/Widgets/WidgetsBundle/WidgetsBundle.swift
@@ -11,7 +11,9 @@ import SwiftUI
 @main
 struct WidgetsBundle: WidgetBundle {
     var body: some Widget {
+        // 순서대로 위젯 목록을 구성합니다.
         MyCardWidget()
+        OpenAppLockScreenWidget()
         QRCodeWidget()
         WidgetsLiveActivity()
     }
